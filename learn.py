0f3e8f03188dcf910a11a59ff7f89453353cc640
allenbiji <georgeallen378@gmail.com>
2024-06-03 19:26:15 +0530

    logic in fucntions

--- learn.py
+++ learn.py
@@ -1,1 +1,11 @@
-print("hello")
+def check_even_list(num_list):
+    even=[]
+    for number in num_list:
+        if number % 2 == 0:
+            even.append(f'{number} is True')
+        else:
+            pass
+    return even
+
+a= check_even_list([1,2,4])
+print(a)
