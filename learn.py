1596d87087c8ec955051af54f009e67d64ad1ebc
allenbiji <georgeallen378@gmail.com>
2024-06-03 22:20:08 +0530

    arguments and keyword arguments

--- learn.py
+++ learn.py
@@ -1,28 +1,5 @@
-from random import shuffle
+def myfunc(*args,**kwargs):
+    return ('I need {} {}'.format(args[2],kwargs['food']))
 
-#create a guessing game
-
-def shuffler(list):
-    shuffle(list)
-    return list
-
-def guess():
-    choice=input("enter your choice")
-    return int(choice)
-
-def guessgame(list,choice):
-    if list[choice]=="0":
-        print("correct guess")
-    else:
-        print("wrong")
-
-
-
-list=[1,3,4,5,7,6,4]
-
-shuffler(list)
-
-a=guess()
-
-guessgame(list,a)
-print(list)
+b=myfunc(20,30,40,food='apple',fav='cow')
+print(b)
