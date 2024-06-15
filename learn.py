a52b565474eaa8ec52b6c99ed2a0f0ae5ff4e546
allenbiji <georgeallen378@gmail.com>
2024-06-15 21:33:48 +0530

    practice 3

--- learn.py
+++ learn.py
@@ -1,8 +1,4 @@
-def same_start_letter(text):
-    words = text.split()  # Split the text into a list of words
-    if len(words) != 2:   # Check if there are exactly two words
-        return False
-    return words[0][0].lower() == words[1][0].lower()  # Compare the first letters
-
+def old_macdonald(name):
+   return name[:3].capitalize() + name[3:].capitalize()
 # Check
-print(same_start_letter("Crazy Kangaroo"))  # Expected output: True
+print(old_macdonald('macdonald'))
