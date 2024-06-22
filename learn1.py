82b6eb17bdad72a835594c439e1e1fe0fc9f9c19
allenbiji <georgeallen378@gmail.com>
2024-06-22 12:18:06 +0530

    eande

--- learn1.py
+++ learn1.py
@@ -1,48 +1,17 @@
-from learn import start
+def ask():
+    while True:
+        try:
 
-start()
-class Account:
-    def __init__(self, owner, balance):
-        self.owner = owner
-        self.balance = balance
-
-    def __str__(self):
-        return (f'Account owner: {self.owner}\n'
-                f'Account Balance: ${self.balance}')
-
-    def deposit(self, number):
-        self.balance += number
-        print("Deposit Accepted")
-        return self.balance
-
-    def withdraw(self, number):
-        if number <= self.balance:
-            self.balance -= number
-            print("Withdrawal accepted")
+            a = int(input("enter an integer"))
+            return a ** 2
+        except TypeError:
+            print("type error ")
+            continue
         else:
-            print('Funds Unavailable')
-
-# 1. Instantiate the class
-acct1 = Account('Jose', 100)
-
-# 2. Print the object
-print(acct1)
-
-# 3. Show the account owner attribute
-acct1.owner
-
-# 4. Show the account balance attribute
-print(acct1.balance)
-
-# 5. Make a series of deposits and withdrawals
-acct1.deposit(50)
-# Deposit Accepted
-acct1.withdraw(75)
-# Withdrawal Accepted
-
-# 6. Make a withdrawal that exceeds the available balance
-acct1.withdraw(500)
-# Funds Unavailable
+            break
+        finally:
+            print('I am always here')
 
 
+ask()
 
