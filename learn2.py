8f7b941d36ba93c67d527e65ceded63607176ffa
allenbiji <georgeallen378@gmail.com>
2024-06-23 12:11:27 +0530

    odd or even

--- learn2.py
+++ learn2.py
@@ -1,4 +1,13 @@
-word="hello planet"
-letters=[*word]
-letters.remove(" ")
-print(len(letters))
+odd=[]
+even=[]
+
+n=int(input('Enter a no. '))
+
+for i in range(1,n+1):
+    if i%2==0:
+        even.append(i)
+    else:
+        odd.append(i)
+
+print(odd)
+print(even)
