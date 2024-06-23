c5253ecfc614e26a7642bc67d1ab99420ab3302b
allenbiji <georgeallen378@gmail.com>
2024-06-23 11:47:31 +0530

    *word

--- learn2.py
+++ learn2.py
@@ -1,13 +1,4 @@
-import random
-
-
-n=random.randint(1,100)
-
-while True:
-    inp=int(input("Enter no."))
-    if inp==n:
-        print("you chose the correct no.")
-        break
-    else:
-        print("wrong guess, try again")
-        continue
+word="hello planet"
+letters=[*word]
+letters.remove(" ")
+print(len(letters))
